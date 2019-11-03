f4ac0f44d6c1a2839a31105d6f510f84c2a14e6f
Think Wu <yingxinwu.g@gmail.com>
2019-11-03 21:22:24 +0800

    fixed backward propagration

--- 02-dnn/week01/03_gradient_check.py
+++ 02-dnn/week01/03_gradient_check.py
@@ -41,8 +41,8 @@ def forward_propagation_n(X, Y, parameters):
     A3 = sigmoid(Z3)
 
     # Cost
-    logprobs = np.multiply(-np.log(A3),Y) + np.multiply(-np.log(1 - A3), 1 - Y)
-    cost = 1./m * np.sum(logprobs)
+    logprobs = np.multiply(-np.log(A3), Y) + np.multiply(-np.log(1 - A3), 1 - Y)
+    cost = np.sum(logprobs) / m
 
     cache = (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3)
 
@@ -59,25 +59,26 @@ def backward_propagation_n(X, Y, cache):
     cache -- cache output from forward_propagation_n()
 
     Returns:
-    gradients -- A dictionary with the gradients of the cost with respect to each parameter, activation and pre-activation variables.
+    gradients -- A dictionary with the gradients of the cost with respect to each parameter,
+        activation and pre-activation variables.
     """
 
     m = X.shape[1]
     (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
 
     dZ3 = A3 - Y
-    dW3 = 1./m * np.dot(dZ3, A2.T)
-    db3 = 1./m * np.sum(dZ3, axis=1, keepdims = True)
+    dW3 = np.dot(dZ3, A2.T) / m
+    db3 = np.sum(dZ3, axis=1, keepdims=True) / m
 
     dA2 = np.dot(W3.T, dZ3)
     dZ2 = np.multiply(dA2, np.int64(A2 > 0))
-    dW2 = 1./m * np.dot(dZ2, A1.T) * 2
-    db2 = 1./m * np.sum(dZ2, axis=1, keepdims = True)
+    dW2 = np.dot(dZ2, A1.T) / m
+    db2 = np.sum(dZ2, axis=1, keepdims=True) / m
 
     dA1 = np.dot(W2.T, dZ2)
     dZ1 = np.multiply(dA1, np.int64(A1 > 0))
-    dW1 = 1./m * np.dot(dZ1, X.T)
-    db1 = 4./m * np.sum(dZ1, axis=1, keepdims = True)
+    dW1 = np.dot(dZ1, X.T) / m
+    db1 = np.sum(dZ1, axis=1, keepdims=True) / m
 
     gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,
                  "dA2": dA2, "dZ2": dZ2, "dW2": dW2, "db2": db2,
@@ -118,14 +119,14 @@ def gradient_check_n(parameters, gradients, X, Y, epsilon=1e-7):
         ### START CODE HERE ### (approx. 3 lines)
         thetaplus = np.copy(parameters_values)                                      # Step 1
         thetaplus[i][0] += epsilon                                # Step 2
-        J_plus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(thetaplus))                                   # Step 3
+        J_plus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(thetaplus))  # Step 3
         ### END CODE HERE ###
 
         # Compute J_minus[i]. Inputs: "parameters_values, epsilon". Output = "J_minus[i]".
         ### START CODE HERE ### (approx. 3 lines)
         thetaminus = np.copy(parameters_values)                                     # Step 1
         thetaminus[i][0] -= epsilon                               # Step 2
-        J_minus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(thetaminus))                                  # Step 3
+        J_minus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(thetaminus))  # Step 3
         ### END CODE HERE ###
 
         # Compute gradapprox[i]
@@ -141,9 +142,13 @@ def gradient_check_n(parameters, gradients, X, Y, epsilon=1e-7):
     ### END CODE HERE ###
 
     if difference > 2e-7:
-        print("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
+        print(
+            "%sThere is a mistake in the backward propagation! difference = %f%s"
+            % ("\033[93m", difference, "\033[0m"))
     else:
-        print("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
+        print(
+            "%sYour backward propagation works perfectly fine! difference = %f%s"
+            % ("\033[92m", difference, "\033[0m"))
 
     return difference
 
